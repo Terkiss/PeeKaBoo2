5763035d1708afc1f4ea41b721034ad5ab5163ed
terkiss <dl2000dl@naver.com>
2020-06-14 02:16:25 +0900

    전송 테스트

--- PeeKaBoo2/app/src/main/java/com/terukiss/peekaboo2/MainActivity.java
+++ PeeKaBoo2/app/src/main/java/com/terukiss/peekaboo2/MainActivity.java
@@ -3,6 +3,9 @@ package com.terukiss.peekaboo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.util.Log;
+import android.util.Xml;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,20 +16,26 @@ import com.terukiss.peekaboo2.helper.JeongLog;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.nio.CharBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
     Socket socket;
     EditText editText;
+    TextView textView;
     JeongLog jeongLog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
 
         jeongLog = new JeongLog(getApplicationContext(), "socketTest");
 
@@ -34,49 +43,69 @@ public class MainActivity extends AppCompatActivity {
 
         editText = findViewById(R.id.editText);
         button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
 
-
-        try {
-            socket = new Socket("192.168.0.25",10000);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 jeongLog.logD("소켓을 테스트 합니다 ");
-                //clickProcess();
+                clickProcess();
             }
         });
 
     }
 
-    private void clickProcess()
-    {
+    private void clickProcess() {
         BufferedReader in = null;
         BufferedWriter out = null;
 
 
+
+
+
         try {
-            in = new BufferedReader( new InputStreamReader(socket.getInputStream()));
-            out = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
+            socket = new Socket("192.168.0.25", 10000);
 
 
-            out.write("서버로 데이터를 전송 할게요 \n");
+           // in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(1024);
 
-            String receiveStr = in.readLine();
+            jeongLog.logD("소켓을 테스트 합니다 ");
+            out.write(editText.getText() + " \n");
+            out.flush();
+            byte[] buf = new byte[200];
+            socket.getInputStream().read(buf);
+           // String receiveStr = in.readLine();
 
-            TextView textView = findViewById(R.id.textView);
+            String aa = "";
+            for (int i = 0; i < buf.length; i++)
+            {
+                if(buf[i]==0)
+                {
+                    byteArrayOutputStream.write(buf, 0, i-1);
+                    aa += byteArrayOutputStream.toString("UTF-8");
+                    break;
+                }
 
-            textView.setText(receiveStr);
 
-            in.close();
+            }
+
+            jeongLog.logD(aa) ;
+
+            textView.setText(aa);
+
+            //textView.setText("receiveStr : "+receiveStr);
+
+            //in.close();
             out.close();
-
+            socket.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
 
     }
 
