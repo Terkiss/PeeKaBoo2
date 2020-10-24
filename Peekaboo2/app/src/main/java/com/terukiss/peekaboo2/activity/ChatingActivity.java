449ce96c8e40e4b4bef3d07c72dd9d6b3109a759
terkiss <dl2000dl@naver.com>
2020-10-25 02:08:59 +0900

    202020

--- Peekaboo2/app/src/main/java/com/terukiss/peekaboo2/activity/ChatingActivity.java
+++ Peekaboo2/app/src/main/java/com/terukiss/peekaboo2/activity/ChatingActivity.java
@@ -2,15 +2,34 @@ package com.terukiss.peekaboo2.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.terukiss.peekaboo2.R;
 
-public class ChatingActivity extends AppCompatActivity {
+public class ChatingActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chating);
+
+        ImageView detailImageView = findViewById(R.id.detailButton); // 사진 전송 문자 전송 동영상 전송 추후 지원 파일전송 등
+
+        EditText editText = findViewById(R.id.inputFilled); // 유저 데이터 입력 공간
+
+        ImageView sendButton  = findViewById(R.id.acceptButton); // 현재 까지 입력된 문자를 처리하는 버튼
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        if(id == R.id d)
     }
 }
